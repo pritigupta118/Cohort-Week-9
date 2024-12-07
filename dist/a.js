5542db7da7a9bcee7bb95d68a8a6f8f9dedda58f
Priti Gupta <civilstudent9900@gmail.com>
2024-12-07 23:55:46 +0530

    3rd commit

--- dist/a.js
+++ dist/a.js
@@ -1,3 +1,14 @@
 "use strict";
-let a = 8;
-console.log(a);
+// let a : number = 8;
+// console.log(a);
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+function doSomething(arg) {
+    console.log(arg);
+}
+doSomething(Direction.Down);
--- src/a.ts
+++ src/a.ts
@@ -1,4 +1,17 @@
-let a : number = 8;
-console.log(a);
+// let a : number = 8;
+// console.log(a);
 
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+function doSomething (arg : Direction) {
+ console.log(arg );
+ 
+}
+
+doSomething (Direction.Down)
 
