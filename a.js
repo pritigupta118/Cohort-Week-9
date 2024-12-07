cd1d1e1c52f58da3d2e762ffa5a858c38212c0fa
Priti Gupta <civilstudent9900@gmail.com>
2024-12-07 23:43:24 +0530

    2nd commit

--- a.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const isLegal = (age) => {
-    if (age > 18)
-        return true;
-    return false;
-};
-console.log(isLegal(11));
---
+++ src/a.ts
@@ -0,0 +1,4 @@
+let a : number = 8;
+console.log(a);
+
+
